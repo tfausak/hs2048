f35d39f995d9a4b81798984c86d4a861a939b36e
Taylor Fausak <taylor@fausak.me>
2014-03-25 22:25:24 -0500

    Add H2048.Grid
    runhaskell haskeleton.hs H2048.Grid

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -4,6 +4,7 @@ import           Criterion.Main    (bgroup, defaultMain)
 import qualified H2048.TileBench
 import qualified H2048.VectorBench
 import qualified H2048Bench
+import qualified H2048.GridBench
 -- HASKELETON: import qualified New.ModuleBench
 
 main :: IO ()
@@ -11,5 +12,6 @@ main = defaultMain
     [ bgroup "H2048" H2048Bench.benchmarks
     , bgroup "H2048.Tile" H2048.TileBench.benchmarks
     , bgroup "H2048.Vector" H2048.VectorBench.benchmarks
+    , bgroup "H2048.Grid" H2048.GridBench.benchmarks
     -- HASKELETON: , bgroup "New.Module" New.ModuleBench.benchmarks
     ]
---
+++ test-suite/H2048/GridSpec.hs
@@ -0,0 +1,7 @@
+module H2048.GridSpec (spec) where
+
+import Test.Hspec
+import H2048.Grid ()
+
+spec :: Spec
+spec = it "is" pending
