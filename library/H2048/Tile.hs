0518ba79eac9ad8cdaa5ec649b3798ad22264733
Taylor Fausak <taylor@fausak.me>
2014-03-25 22:09:07 -0500

    Implement Tile functions

--- library/H2048/Tile.hs
+++ library/H2048/Tile.hs
@@ -1,2 +1,25 @@
 -- | TODO
-module H2048.Tile () where
+module H2048.Tile
+    ( Tile
+    , parse
+    , render
+    , score
+    ) where
+
+-- | TODO
+type Tile = Maybe Int
+
+-- | TODO
+parse :: String -> Tile
+parse "-" = Nothing
+parse s = Just (read s)
+
+-- | TODO
+render :: Tile -> String
+render Nothing = "-"
+render (Just n) = show n
+
+-- | TODO
+score :: Tile -> Int
+score Nothing = 0
+score (Just n) = n
--- test-suite/H2048/TileSpec.hs
+++ test-suite/H2048/TileSpec.hs
@@ -1,7 +1,28 @@
 module H2048.TileSpec (spec) where
 
-import           H2048.Tile ()
+import           H2048.Tile
 import           Test.Hspec
+import           Test.Hspec.QuickCheck
 
 spec :: Spec
-spec = it "is" pending
+spec = do
+    describe "parse" $ do
+        it "returns Nothing for \"-\"" $ do
+            parse "-" `shouldBe` Nothing
+
+        prop "returns Just n for \"n\"" $ do
+            \ n -> parse (show n) == Just n
+
+    describe "render" $ do
+        it "returns \"-\" for Nothing" $ do
+            render Nothing `shouldBe` "-"
+
+        prop "returns \"n\" for Just n" $ do
+            \ n -> render (Just n) == show n
+
+    describe "score" $ do
+        it "returns 0 for Nothing" $ do
+            score Nothing `shouldBe` 0
+
+        prop "returns n for Just n" $ do
+            \ n -> score (Just n) == n
