b10437902d837675caa366b20744ee98ebf8ed60
Taylor Fausak <taylor@fausak.me>
2014-03-27 21:54:42 -0500

    Add play function

--- library/H2048/Main.hs
+++ library/H2048/Main.hs
@@ -1,9 +1,14 @@
 -- | TODO
 module H2048.Main
     ( getMove
+    , play
     ) where
 
+import           Data.Monoid     ((<>))
+import qualified H2048.Board     as B
 import qualified H2048.Direction as D
+import qualified H2048.Game      as G
+import qualified System.Random   as R
 
 -- | TODO
 getMove :: IO (Maybe D.Direction)
@@ -19,3 +24,16 @@ getMove = do
                 'C' -> Just D.East
                 'A' -> Just D.North
                 _ -> Nothing
+
+-- | TODO
+play :: R.RandomGen r => (B.Board, r) -> IO ()
+play (b, r) = do
+    putStrLn ("Score: " <> show (B.score b))
+    putStr (B.render b)
+    if G.isOver b
+        then putStrLn "Game over."
+        else do
+            m <- getMove
+            case m of
+                Nothing -> putStrLn "Invalid move."
+                Just d -> play (G.addRandomTile (B.move b d) r)
