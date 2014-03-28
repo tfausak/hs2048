bc39a87ee8b1f99d2c8ea232628d2272bec47508
Taylor Fausak <taylor@fausak.me>
2014-03-27 22:34:02 -0500

    Fix #6; colorize tiles

--- library/H2048/Tile.hs
+++ library/H2048/Tile.hs
@@ -7,6 +7,8 @@ module H2048.Tile
     , score
     ) where
 
+import Data.Monoid ((<>))
+
 -- | TODO
 type Tile = Maybe Int
 
@@ -21,8 +23,15 @@ parse s = Just (read s)
 
 -- | TODO
 render :: Tile -> String
-render Nothing = "-"
-render (Just n) = show n
+render t = color t <> go t <> reset
+  where
+    color Nothing = "\ESC[30m"
+    color (Just n) = "\ESC[" <> show (30 + f (rank n)) <> "m"
+    f x = if x > 6 then x + 4 else x
+    rank x = floor (logBase 2 (fromIntegral x))
+    go Nothing = "-"
+    go (Just n) = show n
+    reset = "\ESC[0m"
 
 -- | TODO
 score :: Tile -> Int
