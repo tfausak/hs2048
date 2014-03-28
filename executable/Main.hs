e08845c904d6ef51cce851505ef19871d8242337
Taylor Fausak <taylor@fausak.me>
2014-03-27 22:09:31 -0500

    Fix #4; write executable

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,6 +1,12 @@
 module Main (main) where
 
-import           H2048 ()
+import           H2048         (new, play)
+import           System.IO     as IO
+import           System.Random (getStdGen)
 
 main :: IO ()
-main = return ()
+main = do
+    IO.hSetBuffering IO.stdin IO.NoBuffering
+
+    r <- getStdGen
+    play (new r)
