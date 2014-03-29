3b6c0a7a81de70fb5ea4d14d3f98186733c6627a
Taylor Fausak <taylor@fausak.me>
2014-03-29 16:49:21 -0500

    Refactor H2048.Vector

--- library/H2048/Vector.hs
+++ library/H2048/Vector.hs
@@ -1,4 +1,4 @@
--- | TODO
+-- | Types and functions for manipulating vectors.
 module H2048.Vector
     ( Vector
     , canShift
@@ -11,46 +11,99 @@ module H2048.Vector
     , shift
     ) where
 
-import           Data.List   (group, intercalate)
-import           Data.Maybe  (isJust)
-import           Data.Maybe  (isNothing)
+import           Data.List   (group)
+import           Data.Maybe  (isJust, isNothing)
 import           Data.Monoid ((<>))
 import qualified H2048.Tile  as T
 
--- | TODO
+{- |
+    Represents a row or column on the game board. By convention, a vector has
+    4 tiles.
+-}
 type Vector = [T.Tile]
 
--- | TODO
+{- |
+    Determines if the vector can be shifted.
+
+    >>> canShift [Nothing, Just 2, Nothing, Nothing]
+    True
+-}
 canShift :: Vector -> Bool
 canShift v = shift v /= v
 
--- | TODO
+{- |
+    Returns an empty vector of the given size.
+
+    >>> empty 4
+    [Nothing,Nothing,Nothing,Nothing]
+-}
 empty :: Int -> Vector
 empty = flip replicate T.empty
 
--- | TODO
+{- |
+    Returns the indexes that don't contain tiles.
+
+    >>> emptyIndexes [Nothing, Just 2, Nothing, Nothing]
+    [0,2,3]
+-}
 emptyIndexes :: Vector -> [Int]
 emptyIndexes = fmap fst . filter (isNothing . snd) . zip [0 ..]
 
--- | TODO
+{- |
+    Parses a string as a vector. This is the inverse of 'render'.
+
+    >>> parse "- 2 - -"
+    [Nothing,Just 2,Nothing,Nothing]
+-}
 parse :: String -> Vector
 parse = fmap T.parse . words
 
--- | TODO
-render :: Vector -> String
-render = intercalate "\t" . fmap T.render
+{- |
+    Renders a vector as a string. This is the inverse of 'parse'.
 
--- | TODO
+    >>> render [Nothing, Just 2, Nothing, Nothing]
+    "- 2 - -"
+-}
+render :: Vector -> String
+render = unwords . fmap T.render
+
+{- |
+    Calculates the score of a vector.
+
+    >>> score [Nothing, Just 2, Just 4, Just 8]
+    20
+-}
 score :: Vector -> Int
 score = sum . fmap T.score
 
--- | TODO
+{- |
+    Sets a tile at the given index in the vector.
+
+    >>> set [Nothing, Nothing, Nothing, Nothing] (Just 2) 1
+    [Nothing,Just 2,Nothing,Nothing]
+-}
 set :: Vector -> T.Tile -> Int -> Vector
 set v t i = zipWith go [0 ..] v
   where
     go i' t' = if i' == i then t else t'
 
--- | TODO
+{- |
+    Shifts a vector toward the head. The output vector will be the same size as
+    the input vector, padded with @Nothing@.
+
+    >>> shift [Nothing, Just 2, Nothing, Nothing]
+    [Just 2,Nothing,Nothing,Nothing]
+
+    Like tiles will be combined.
+
+    >>> shift [Just 2, Nothing, Just 2, Just 2]
+    [Just 4,Just 2,Nothing,Nothing]
+
+    Any number of tiles can be combined in one shift.
+
+    >>> shift [Just 2, Just 2, Just 4, Just 4]
+    [Just 4,Just 8,Nothing,Nothing]
+-}
 shift :: Vector -> Vector
 shift v = take n (v' <> empty n)
   where
