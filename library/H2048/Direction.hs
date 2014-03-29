37debe31902ac07c6d7e3534e07a3079e36329fe
Taylor Fausak <taylor@fausak.me>
2014-03-29 17:08:39 -0500

    Refactor H2048.Direction

--- library/H2048/Direction.hs
+++ library/H2048/Direction.hs
@@ -1,12 +1,25 @@
--- | TODO
+-- | Types and functions for manipulating directions.
 module H2048.Direction
     ( Direction (..)
+    , directions
     ) where
 
--- | TODO
+{- |
+    Represents a direction that the game board can be moved in. The cardinal
+    directions are used to avoid collisions with 'Left' and 'Right'.
+-}
 data Direction
     = West
     | South
     | East
     | North
     deriving (Bounded, Enum, Eq, Show)
+
+{- |
+    Returns all of the directions.
+
+    >>> directions
+    [West,South,East,North]
+-}
+directions :: [Direction]
+directions = [minBound ..]
--- test-suite/H2048/DirectionSpec.hs
+++ test-suite/H2048/DirectionSpec.hs
@@ -1,7 +1,33 @@
 module H2048.DirectionSpec (spec) where
 
-import           H2048.Direction ()
+import           H2048.Direction
 import           Test.Hspec
 
 spec :: Spec
-spec = it "is" pending
+spec = do
+    describe "Direction" $ do
+        it "is an instance of Bounded" $ do
+            minBound `shouldBe` West
+            maxBound `shouldBe` North
+
+        it "is an instance of Enum" $ do
+            succ West `shouldBe` South
+            pred North `shouldBe` East
+            toEnum 1 `shouldBe` South
+            fromEnum East `shouldBe` 2
+            enumFrom North `shouldBe` [North]
+            enumFromThen West East `shouldBe` [West, East]
+            enumFromTo West West `shouldBe` [West]
+            enumFromThenTo West East North `shouldBe` [West, East]
+
+        it "is an instance of Eq" $ do
+            West == West `shouldBe` True
+            West /= West `shouldBe` False
+
+        it "is an instance of Show" $ do
+            show West `shouldBe` "West"
+            showList [West] "" `shouldBe` "[West]"
+
+    describe "directions" $ do
+        it "returns the directions" $ do
+            directions `shouldBe` [West, South, East, North]
