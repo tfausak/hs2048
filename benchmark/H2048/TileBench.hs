8d2457cbf16376825568d4453c1bae7d262f6a20
Taylor Fausak <taylor@fausak.me>
2014-03-29 10:50:15 -0500

    Refactor H2048.Tile

--- benchmark/H2048/TileBench.hs
+++ benchmark/H2048/TileBench.hs
@@ -1,7 +1,29 @@
 module H2048.TileBench (benchmarks) where
 
 import           Criterion
-import           H2048.Tile ()
+import           H2048.Tile
 
 benchmarks :: [Benchmark]
-benchmarks = []
+benchmarks =
+    [ bgroup "empty"
+        [ bench "" $ nf (const empty) ()
+        ]
+    , bgroup "parse"
+        [ bench "-" $ nf parse "-"
+        , bench "2" $ nf parse "2"
+        ]
+    , bgroup "rank"
+        [ bench "Nothing" $ nf rank Nothing
+        , bench "Just 2" $ nf rank (Just 2)
+        , bench "Just 2048" $ nf rank (Just 2048)
+        ]
+    , bgroup "render"
+        [ bench "Nothing" $ nf render Nothing
+        , bench "Just 2" $ nf render (Just 2)
+        ]
+    , bgroup "score"
+        [ bench "Nothing" $ nf score Nothing
+        , bench "Just 2" $ nf score (Just 2)
+        , bench "Just 2048" $ nf score (Just 2048)
+        ]
+    ]
