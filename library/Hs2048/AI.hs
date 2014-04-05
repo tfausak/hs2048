878fde02de0301dfc9b1ba27c64473e8f273ad58
Taylor Fausak <taylor@fausak.me>
2014-04-05 12:17:45 -0500

    Also consider smoothness
    This is extremely ineffecient, but it is the only strategy that's won so
    far.

--- library/Hs2048/AI.hs
+++ library/Hs2048/AI.hs
@@ -7,10 +7,13 @@ module Hs2048.AI
     ) where
 
 import           Data.List        (genericLength, group, maximumBy, sort)
-import           Data.Maybe       (catMaybes)
+import           Data.Maybe       (catMaybes, isJust)
 import           Data.Ord         (comparing)
 import qualified Hs2048.Board     as B
 import qualified Hs2048.Direction as D
+import qualified Hs2048.Tile as T
+import qualified Hs2048.Vector as V
+import qualified Hs2048.Point as P
 
 {- |
     Calculates the average value of a list.
@@ -74,8 +77,55 @@ moves b = fmap go ds
 -}
 quality :: B.Board -> Int
 quality b = sum
-    [ 1 * B.score b
+    [ 0
+    , 1 * B.score b
     , 1 * length (moves b)
     , 1 * length (B.emptyPoints b)
     , -1 * length (duplicates b)
+    , -1 * smoothness b
     ]
+
+--
+
+neighbors :: B.Board -> P.Point -> [P.Point]
+neighbors b (x, y) = filter (inBounds b)
+    [ (x - 1, y    )
+    , (x + 1, y    )
+    , (x    , y - 1)
+    , (x    , y + 1)
+    ]
+
+inBounds :: B.Board -> P.Point -> Bool
+inBounds b (x, y) =
+    x >= 0 &&
+    y >= 0 &&
+    y < height &&
+    x < width
+  where
+    height = length b
+    width = if null b then 0 else length (head b)
+
+difference :: T.Tile -> T.Tile -> Int
+difference a b = T.rank a - T.rank b
+
+get :: B.Board -> P.Point -> T.Tile
+get b (x, y) = b !! y !! x
+
+points :: B.Board -> [P.Point]
+points b =
+    [ (x, y)
+    | x <- [0 .. width - 1]
+    , y <- [0 .. height - 1]
+    ]
+  where
+    height = length b
+    width = if null b then 0 else length (head b)
+
+edges :: B.Board -> [(P.Point, P.Point)]
+edges b = concatMap (\ p -> zip (repeat p) (neighbors b p)) (points b)
+
+weight :: B.Board -> (P.Point, P.Point) -> Int
+weight b (p1, p2) = abs (difference (get b p1) (get b p2))
+
+smoothness :: B.Board -> Int
+smoothness b = sum (map (weight b) (edges b))
